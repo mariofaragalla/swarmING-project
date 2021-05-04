a0b3c9dac6fff40873e5c07ab6eaac44b3c1fb73
com2440 <mario.faragalla@tuhh.de>
2021-05-04 17:40:55 +0200

    #1.2

---
+++ catkin_ws/src/iq_gnc/src/square (original).cpp
@@ -0,0 +1,82 @@
+#include <gnc_functions.hpp>		//add all functions that are associated with the control api
+//include API 
+
+int main(int argc, char** argv)
+{
+	//initialize ros 
+	//Every time you have to run a ros node you must add these 2 functions
+	ros::init(argc, argv, "gnc_node");	//here we name the node gnc_node
+	ros::NodeHandle gnc_node("~");	// the node handle is the way that you can access the communications within ros
+	
+	//initialize control publisher/subscribers
+	init_publisher_subscriber(gnc_node);	//This function takes our ros node handle as an input and initializes subcribers that will collect the necessary information from our autopilot.
+
+  	// wait for FCU connection  (loop until the node can communicate with the drone's flight control unit (FCU))
+	wait4connect();
+
+	//wait for used to switch to mode GUIDED (hold the program until the pilot executes the program by switching the FCU flight mode to GUIDED)
+	wait4start();
+
+	//create local reference frame 
+	initialize_local_frame();
+
+	//request takeoff
+	takeoff(3);
+
+	//specify some waypoints 
+	std::vector<gnc_api_waypoint> waypointList;
+	gnc_api_waypoint nextWayPoint;
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 5;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = -90;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 5;
+	nextWayPoint.y = 5;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 5;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 90;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 180;
+	waypointList.push_back(nextWayPoint);
+	nextWayPoint.x = 0;
+	nextWayPoint.y = 0;
+	nextWayPoint.z = 3;
+	nextWayPoint.psi = 0;
+	waypointList.push_back(nextWayPoint);
+
+
+	//specify control loop rate. We recommend a low frequency to not over load the FCU with messages. Too many messages will cause the drone to be sluggish
+	ros::Rate rate(2.0);
+	int counter = 0;
+	while(ros::ok())
+	{
+		ros::spinOnce();
+		rate.sleep();
+		if(check_waypoint_reached(.3) == 1)
+		{
+			if (counter < waypointList.size())
+			{
+				set_destination(waypointList[counter].x,waypointList[counter].y,waypointList[counter].z, waypointList[counter].psi);
+				counter++;	
+			}else{
+				//land after all waypoints are reached
+				land();
+			}	
+		}	
+		
+	}
+	return 0;
+}
